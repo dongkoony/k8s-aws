2ca128e1d6a2f02759b5acc63cf57b9fff0997e8
donghyeon shin <donghyun4591@gmail.com>
2024-10-31 17:11:44 +0900

    Outputs 출력 내용 변경

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,28 @@
 # ./outputs.tf
 
-# master 스냅샷 ID 출력
+output "master_ip" {
+  description = "Master node public IP"
+  value       = aws_instance.k8s_master.public_ip
+}
+
+output "worker_ips" {
+  description = "Worker nodes public IPs"
+  value = {
+    node1 = aws_instance.k8s_workers[0].public_ip
+    node2 = aws_instance.k8s_workers[1].public_ip
+  }
+}
+
+# 필요한 경우 스냅샷 ID도 출력할 수 있습니다
 output "master_snapshot_id" {
-  value = aws_ebs_snapshot.k8s_snapshots["master"].id
+  description = "Master node EBS snapshot ID"
+  value       = aws_ebs_snapshot.master_snapshot.id
 }
 
-# node1 스냅샷 ID 출력
-output "node1_snapshot_id" {
-  value = aws_ebs_snapshot.k8s_snapshots["node1"].id
-}
-
-# node2 스냅샷 ID 출력
-output "node2_snapshot_id" {
-  value = aws_ebs_snapshot.k8s_snapshots["node2"].id
+output "worker_snapshot_ids" {
+  description = "Worker nodes EBS snapshot IDs"
+  value = {
+    node1 = aws_ebs_snapshot.worker_snapshots[0].id
+    node2 = aws_ebs_snapshot.worker_snapshots[1].id
+  }
 }
