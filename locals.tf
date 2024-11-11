95a0ccfac6b3f7ba3469921708f5ab4b13caa8fb
donghyeonshin <donghyun4591@gmail.com>
2024-11-11 14:42:55 +0900

    locals 스냅샷 주석처리

--- locals.tf
+++ locals.tf
@@ -1,10 +1,26 @@
-locals {
-  snapshot_ids = {
-    master = length(data.aws_ebs_snapshot.master_snapshot) > 0 ? data.aws_ebs_snapshot.master_snapshot[0].id : null,
-    node1  = length(data.aws_ebs_snapshot.node1_snapshot) > 0 ? data.aws_ebs_snapshot.node1_snapshot[0].id : null,
-    node2  = length(data.aws_ebs_snapshot.node2_snapshot) > 0 ? data.aws_ebs_snapshot.node2_snapshot[0].id : null
-  }
-}
+# locals {
+#   snapshot_ids = {
+#     master = try(data.aws_ebs_snapshot.master_snapshot.id, null)
+#     node1  = try(data.aws_ebs_snapshot.node1_snapshot.id, null)
+#     node2  = try(data.aws_ebs_snapshot.node2_snapshot.id, null)
+#   }
+# }
+
+# # 스냅샷 존재 여부 확인을 위한 locals
+# locals {
+#   master_snapshot_filter = {
+#     name   = "tag:Name"
+#     values = ["k8s-master-snapshot"]
+#   }
+#   node1_snapshot_filter = {
+#     name   = "tag:Name"
+#     values = ["k8s-node1-snapshot"]
+#   }
+#   node2_snapshot_filter = {
+#     name   = "tag:Name"
+#     values = ["k8s-node2-snapshot"]
+#   }
+# }
 
 locals {
   key_destination = "/home/ubuntu/${var.key_name}"
